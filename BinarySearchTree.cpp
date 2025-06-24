e0ab2be4be1656e51f71f726983f61cb6d3b4dc9
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-24 11:09:05 +0700

    menambahkan logika pohon kosong, node baru menjadi root

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -48,6 +48,16 @@ public:
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
 
+        // Step 5: if parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a: Mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b: Exit
+            return;
+        }
+
         
     }
     
