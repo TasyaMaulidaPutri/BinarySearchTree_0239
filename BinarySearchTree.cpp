f06f4e50ec6fafd383819b5e0ec8be04eb5cee05
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-24 10:34:32 +0700

    Assign nilai dan set pointer kiri kanan menjadi NULL

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,5 +35,17 @@ public:
         // Step 1: Allocate memory for the new node
         Node *newNode = new Node();
 
+        
+        // Step 2: Assign value to the data field of new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        
+
+        
+
     }
 };
